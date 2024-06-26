1c3fd0fd4971b988d9c6269cb877cfc4c4703d94
Alexdudley95 <alexandr.dudley95@gmail.com>
2024-06-26 19:26:12 +0100

    compiled to jar

--- src/main/java/com/example/auctionimagerenamer/Main.java
+++ src/main/java/com/example/auctionimagerenamer/Main.java
@@ -23,7 +23,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
 
         Label label = new Label();
         label.setText("Input lot num:");
